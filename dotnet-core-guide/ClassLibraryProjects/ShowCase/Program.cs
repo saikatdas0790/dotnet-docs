8bacc7b49fc7e454debf1c5c8f958cdf053b34d6
Saikat Das <saikatdas0790@gmail.com>
2018-01-07 02:28:33 +0530

    Complete 'Get Started' section
    Set up console application, class library, unit test project, publish to the filesystem and generate Nuget package

---
+++ dotnet-core-guide/ClassLibraryProjects/ShowCase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UtilityLibraries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = Console.WindowHeight;
+
+            Console.Clear();
+
+            do
+            {
+                if (Console.CursorTop >= rows || Console.CursorTop == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nPress <Enter> only to exit; otherwise, enter a string and press <Enter>:\n");
+                }
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) break;
+                Console.WriteLine($"Input: {input} {"Begins with uppercase? ", 30}: {(input.StartsWithUpper() ? "Yes": "No")}\n");
+
+            } while (true);
+        }
+    }
+}
---
+++ dotnet-core-guide/ClassLibraryProjects/StringLibraryTest/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtilityLibraries
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestStartsWithUpper()
+        {
+            // Tests that we expect to return true.
+            string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsTrue(result, string.Format($"Expected for '{word}': true; Actual: {result}"));
+            }
+        }
+
+        [TestMethod]
+        public void TestDoesNotStartWithUpper()
+        {
+            //Tests that we expect to return false.
+            string[] words = { "alphabet", "zebra", "abc", "αυτοκινητοβιομηχανία", "государство",
+                               "1234", ".", ";", " " };
+
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsFalse(result, string.Format($"Expected for '{word}': false; Actual: {result}"));
+            }
+        }
+
+        [TestMethod]
+        public void DirectCallWithNullOrEmpty()
+        {
+            // Tests that we expect to return false.
+            string[] words = { string.Empty, null };
+            foreach (var word in words)
+            {
+                bool result = StringLibrary.StartsWithUpper(word);
+                Assert.IsFalse(result, string.Format($"Expected for '{word}': false; Actual: {result}"));
+            }
+        }
+    }
+}
