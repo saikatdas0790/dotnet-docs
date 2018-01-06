ff8b48071d5b16bc9a07f8c9a0edc9a5c14e9cf4
Saikat Das <saikatdas0790@gmail.com>
2018-01-07 02:29:39 +0530

    Complete 'Quick Starts' section
    Write the Fibonacci Number generator

---
+++ csharp-guide/quick-starts/FibonacciNumberGenerator/FibonacciNumberGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciNumberGenerator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var item in GenerateFibonacciList(20))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static List<int> GenerateFibonacciList(int size)
+        {
+            var listOfNumbers = new List<int>() { 1, 1 };
+
+            for (int i = 2; i < size; i++)
+                listOfNumbers.Add(listOfNumbers[i - 2] + listOfNumbers[i - 1]);
+
+            return listOfNumbers;
+        }
+    }
+}
