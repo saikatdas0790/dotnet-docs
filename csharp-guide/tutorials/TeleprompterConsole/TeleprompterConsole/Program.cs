b685077387b7070513ad5f673e50b5d954bd2697
Saikat Das <saikatdas0790@gmail.com>
2018-01-12 09:37:39 +0530

    Add project for Teleprompter and 'lock' keyword

---
+++ csharp-guide/tutorials/TeleprompterConsole/TeleprompterConsole/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TeleprompterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var lines = ReadFrom("sampleQuotes.txt");
+            //foreach (var line in lines)
+            //{
+            //    Console.WriteLine(line);
+            //    if (!string.IsNullOrWhiteSpace(line))
+            //    {
+            //        var pause = Task.Delay(200);
+            //        // Synchronously waiting on a task is an
+            //        // anti-pattern. This will get fixed in later
+            //        // steps.
+            //        pause.Wait();
+            //    }
+            //}
+            RunTeleprompter().Wait();
+        }
+
+        private static async Task RunTeleprompter()
+        {
+            var config = new TelePrompterConfig();
+            var displayTask = ShowTeleprompter(config);
+
+            var speedTask = GetInput(config);
+            await Task.WhenAny(displayTask, speedTask);
+        }
+
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var words = line.Split(' ');
+                    var lineLength = 0;
+                    foreach (var word in words)
+                    {
+                        yield return word + " ";
+                        lineLength += word.Length + 1;
+                        if (lineLength > 70)
+                        {
+                            yield return Environment.NewLine;
+                            lineLength = 0;
+                        }
+                    }
+                    //yield return Environment.NewLine;
+                }
+            }
+        }
+
+        private static async Task ShowTeleprompter(TelePrompterConfig config)
+        {
+            var words = ReadFrom("sampleQuotes.txt");
+            foreach (var line in words)
+            {
+                Console.WriteLine(line);
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    await Task.Delay(config.DelayInMilliseconds);
+                }
+            }
+        }
+
+        private static async Task GetInput(TelePrompterConfig config)
+        {
+            Action work = () =>
+            {
+                do
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.KeyChar == '>')
+                        config.UpdateDelay(-10);
+                    else if (key.KeyChar == '<')
+                        config.UpdateDelay(10);
+                } while (!config.Done);
+            };
+            await Task.Run(work);
+        }
+    }
+}
---
+++ csharp-guide/tutorials/TeleprompterConsole/TeleprompterConsole/TelePrompterConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Math;
+
+namespace TeleprompterConsole
+{
+    class TelePrompterConfig
+    {
+        private object lockHandle = new object();
+        public int DelayInMilliseconds { get; private set; } = 200;
+
+        public void UpdateDelay(int increment) //negative to speed up
+        {
+            var newDelay = Min(DelayInMilliseconds + increment, 1000);
+            newDelay = Max(newDelay, 20);
+            lock (lockHandle)
+            {
+                DelayInMilliseconds = newDelay;
+            }
+        }
+
+        public bool Done => done;
+
+        private bool done;
+
+        public void SetDone()
+        {
+            done = true;
+        }
+    }
+}
