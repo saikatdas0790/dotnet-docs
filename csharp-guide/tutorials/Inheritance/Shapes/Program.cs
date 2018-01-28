760f5b715205f8696adcf87c4814ac1b7e128f49
Saikat Das <saikatdas0790@gmail.com>
2018-01-28 13:43:51 +0530

    Add Shapes example to show abstract base class inheritance

---
+++ csharp-guide/tutorials/Inheritance/Shapes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+                
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = { new Rectangle(10, 12), new Square(5), new Circle(3) };
+            foreach (var shape in shapes)
+            {
+                Console.WriteLine($"{shape}: area, {Shape.GetArea(shape)}; perimeter, {Shape.GetPerimeter(shape)}");
+                var rect = shape as Rectangle;
+                if (rect != null)
+                {
+                    Console.WriteLine($"    Is Square: {rect.IsSquare()}, Diagonal: {rect.Diagonal}");
+                    continue;
+                }
+
+                var sq = shape as Square;
+                if (sq != null)
+                {
+                    Console.WriteLine($"    Diagonal: {sq.Diagonal}");
+                    continue;
+                }
+            }
+        }
+    }
+}
